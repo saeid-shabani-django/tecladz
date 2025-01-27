659ec8b3904e6c3393c04390f7e122a613a58ffc
saeid shabani <django_saeid.sh@proton.me>
2025-01-27 19:31:18 +0330

    make payment process and pyment history successfully

--- payment/views.py
+++ payment/views.py
@@ -4,25 +4,26 @@ from rest_framework import status
 from rest_framework.response import Response
 import requests
 import json
-
-
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view
 
 def payment(request):
     customer_id = request.user.id
-    order_id = Order.objects.get(customer_id=customer_id).id
+    order_id = Order.objects.get(customer_id=customer_id,status='unpaid').id
     order = get_object_or_404(Order,id=order_id)
     total_price = order.get_total_price()
+    print(total_price)
 
     zarinpal_request_for_post = (
-        "https://payment.zarinpal.com/pg/v4/payment/request.json"
+        "https://sandbox.zarinpal.com/pg/v4/payment/request.json"
     )
 
 
     request_data = {
         "merchant_id": "07d2feec-2010-4bb2-a380-f0f4d4075453",
         "amount": total_price,
-        "callback_url": reverse('payment_callback'),
-        "description": f"tecladz transaction for the user:  {request.user.username}",
+        "callback_url": request.build_absolute_uri(reverse('payment_callback')),
+        "description": f"tecladz transaction for the user:  {request.user.email}",
         "metadata": {"mobile": "09109555234", "email": "info.test@example.com"},
     }
     header = {
@@ -40,12 +41,12 @@ def payment(request):
     authority = data["authority"]
     order.authority_from_zarinpal = authority
     order.save()
-    if (len(resp.json()["errors"] == 0)) or (not resp.json()["errors"]):
+    if not resp.json()["errors"]:
         return redirect(f"https://sandbox.zarinpal.com/pg/StartPay/{authority}")
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
     
-
+@api_view(['GET','POST'])
 def payment_callback_view(request):
     payment_authority = request.GET.get('Authority')
     payment_status = request.GET.get('Status')
@@ -63,7 +64,7 @@ def payment_callback_view(request):
         }
         resp = requests.post(url='https://sandbox.zarinpal.com/pg/v4/payment/verify.json',data=json.dumps(request_data),headers=header)
         data = resp.json()["data"]
-        if 'data' in resp.json() and (len(resp.json()["data"]["errors"] == 0)) or (not resp.json()["data"]["errors"]):
+        if 'data' in resp.json():
             data = resp.json()["data"]
             payment_code = data['code']
             if payment_code == 100:
@@ -71,14 +72,15 @@ def payment_callback_view(request):
                 order.ref_id_from_zarinpal = data['ref_id']
                 order.data_from_zarinpal = data
                 order.save()
-return Response('the payment process was successful',status=status.HTTP_200_OK)
+                return Response('the payment process was successful',status=status.HTTP_200_OK)
             elif payment_code == 101:
                 return Response('the payment has been proceed before',status=status.HTTP_200_OK)
             else:
-                error_code = resp.json()['errors']['code']
-                error_message = resp.json()['errors']['message']
-                return Response(f'{error_message} {error_code}',status=status.HTTP_401_UNAUTHORIZED)
-
+                # error_code = resp.json()['errors']['code']
+                # error_message = resp.json()['errors']['message']
+                return Response('payment failed',status=status.HTTP_401_UNAUTHORIZED)
+    if payment_status=='NOK':
+                return Response('payment failed',status=status.HTTP_401_UNAUTHORIZED)
 
 
 
